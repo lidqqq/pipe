8ffee25b67df207d6d637c9b116fa917a5eb3007
lidqqq <lidqqq.dev@gmail.com>
2021-01-25 09:49:10 +0900

    :dog: chore

--- src/pipe.ts
+++ src/pipe.ts
@@ -14,10 +14,10 @@ type Pipe = {
   <
     T1 extends (...args: any) => any,
     T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
+      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : never
     ) => any,
     T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
+      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : never
     ) => any
   >(
     fn1: T1,
@@ -27,13 +27,13 @@ type Pipe = {
   <
     T1 extends (...args: any) => any,
     T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
+      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : never
     ) => any,
     T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
+      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : never
     ) => any,
     T4 extends (
-      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : ReturnType<T3>
+      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : never
     ) => any
   >(
     fn1: T1,
@@ -44,16 +44,16 @@ type Pipe = {
   <
     T1 extends (...args: any) => any,
     T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
+      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : never
     ) => any,
     T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
+      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : never
     ) => any,
     T4 extends (
-      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : ReturnType<T3>
+      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : never
     ) => any,
     T5 extends (
-      arg: ReturnType<T4> extends Promise<infer T4> ? T4 : ReturnType<T4>
+      arg: ReturnType<T4> extends Promise<infer T4> ? T4 : never
     ) => any
   >(
     fn1: T1,
@@ -68,23 +68,14 @@ type PipeSync = {
   <T1 extends (...args: any) => any>(fn1: T1): (
     ...x: Parameters<T1>
   ) => ReturnType<T1>;
-  <
-    T1 extends (...args: any) => any,
-    T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
-    ) => any
-  >(
+  <T1 extends (...args: any) => any, T2 extends (arg: ReturnType<T1>) => any>(
     fn1: T1,
     fn2: T2
   ): (...x: Parameters<T1>) => ReturnType<T2>;
   <
     T1 extends (...args: any) => any,
-    T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
-    ) => any,
-    T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
-    ) => any
+    T2 extends (arg: ReturnType<T1>) => any,
+    T3 extends (arg: ReturnType<T2>) => any
   >(
     fn1: T1,
     fn2: T2,
@@ -92,15 +83,9 @@ type PipeSync = {
   ): (...x: Parameters<T1>) => ReturnType<T3>;
   <
     T1 extends (...args: any) => any,
-    T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
-    ) => any,
-    T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
-    ) => any,
-    T4 extends (
-      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : ReturnType<T3>
-    ) => any
+    T2 extends (arg: ReturnType<T1>) => any,
+    T3 extends (arg: ReturnType<T2>) => any,
+    T4 extends (arg: ReturnType<T3>) => any
   >(
     fn1: T1,
     fn2: T2,
@@ -109,18 +94,10 @@ type PipeSync = {
   ): (...x: Parameters<T1>) => ReturnType<T4>;
   <
     T1 extends (...args: any) => any,
-    T2 extends (
-      arg: ReturnType<T1> extends Promise<infer T1> ? T1 : ReturnType<T1>
-    ) => any,
-    T3 extends (
-      arg: ReturnType<T2> extends Promise<infer T2> ? T2 : ReturnType<T2>
-    ) => any,
-    T4 extends (
-      arg: ReturnType<T3> extends Promise<infer T3> ? T3 : ReturnType<T3>
-    ) => any,
-    T5 extends (
-      arg: ReturnType<T4> extends Promise<infer T4> ? T4 : ReturnType<T4>
-    ) => any
+    T2 extends (arg: ReturnType<T1>) => any,
+    T3 extends (arg: ReturnType<T2>) => any,
+    T4 extends (arg: ReturnType<T3>) => any,
+    T5 extends (arg: ReturnType<T4>) => any
   >(
     fn1: T1,
     fn2: T2,
